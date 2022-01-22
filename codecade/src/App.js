2fba43b9ec273fb9924241a2ec81c8be2194734f
tore88 <sa.muredda43@gmail.com>
2022-01-22 23:42:38 +0000

    app created

--- codecade/src/App.js
+++ codecade/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { animals } from "./animals";
+import React from "react";
+import style from "./App.module.css";
+import backImage from "./ocean.jpg";
 
 function App() {
+  const title = ``;
+  const images = [];
+  const background = (
+    <img className={style.background} src={backImage} alt="ocean" />
+  );
+  function myFunction(e) {
+    const selectAnimal = e.target.alt;
+    const animalInfo = animals[selectAnimal];
+    const randomIndex = Math.floor(Math.random() * animalInfo.facts.length);
+    const funFact = animalInfo.facts[randomIndex];
+    document.getElementById("fact").innerHTML = funFact;
+  }
+
+  for (const animal in animals) {
+    <div className={style.fact}>
+      {images.push(
+        <img
+          key={animal}
+          className={style.animal}
+          alt={animal}
+          src={animals[animal].image}
+          aria-label={animal}
+          role="button"
+          onClick={myFunction}
+        />
+      )}
+    </div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title === `` ? `Click an animal for a fun fact` : title}</h1>
+      {background}
+      <p id="fact"></p>
+      <div className={style.animals}>{images}</div>
     </div>
   );
 }
---
+++ codecade/src/animals.js
@@ -0,0 +1,30 @@
+import dolphin from "./dolphin.jpg";
+import lobster from "./lobster.jpg";
+import starfish from "./starfish.jpg";
+
+export const animals = {
+  dolphin: {
+    image: dolphin,
+    facts: [
+      "Dolphins have been shown to give distinct names to each other!",
+      "Dolphins are known to display their own culture!",
+      "Dolphins have two stomachs!",
+    ],
+  },
+  lobster: {
+    image: lobster,
+    facts: [
+      "Lobsters taste with their legs!",
+      "Lobsters chew with their stomachs!",
+      "Lobsters can live as long as 100 years.",
+    ],
+  },
+  starfish: {
+    image: starfish,
+    facts: [
+      "Starfish can have up to 40 arms!",
+      "Starfish have no brain and no blood!",
+      "Starfish can regenerate their own arms!",
+    ],
+  },
+};
